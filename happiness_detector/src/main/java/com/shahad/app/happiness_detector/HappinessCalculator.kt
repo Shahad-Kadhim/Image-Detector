6899f6159cb758acc2c0dc61e3e3cd180ad89d8e
Shahad Kadhim <almuhandisa.sh@gmail.com>
2021-12-28 13:32:48 +0300

    edit sadness minimum

--- happiness_detector/src/main/java/com/shahad/app/happiness_detector/HappinessCalculator.kt
+++ happiness_detector/src/main/java/com/shahad/app/happiness_detector/HappinessCalculator.kt
@@ -58,7 +58,7 @@ class HappinessCalculator(
         val storedHappyLabels = arrayListOf("comics","circus","smile","laugh","balloon","picnic","clown","christmas","dance","santa claus","thanksgiving","vacation","love","money","shikoku","pet","pizza","lipstick","cool","duck","turtle","dog","rainbow","flower","airplane","butterfly","marathon","cake","fireworks","baby","bride","joker","selfie","dress","fun","leisure","river","blessed","parturition","birth","occasion","joyous","lighthearted","celebration","carnival","party")
         val storeSadLabels = arrayListOf("bullfighting","junk","shipwreck","caving","jungle","fire","cairn terrier","forest","militia","volcano","rocket","bangs","lightning","army","storm","helmet","funeral","sad","awful","burial","dead","depressing","farewell","misery","depression","pain","upset","torture","battle","combat","blood","fire","flood","hospital","weapon","gun","monster","fear","horror","accident","cry","tears","dark")
         const val MAXIMUM_SADNESS = 0.49
-        const val MINIMUM_SADNESS = 0.0
+        const val MINIMUM_SADNESS = 0.01
         const val MAXIMUM_HAPPINESS = 1.0
         const val MINIMUM_HAPPINESS = 0.6
     }
